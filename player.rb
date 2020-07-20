d809c40ed65ba9120f73e18130c2bdbea00e734a
John Saputo <jsaputo1@gmail.com>
2020-07-20 22:55:32 +0000

    added lives and name to Player class

--- player.rb
+++ player.rb
@@ -1,1 +1,12 @@
-puts "player working"
+class Player
+
+  attr_accessor :lives, :name
+  def initialize(name)
+    @lives = 3
+    @name = name
+  end
+
+# test = Player.new('Player1')
+# puts test.name
+# puts test.lives
+end
