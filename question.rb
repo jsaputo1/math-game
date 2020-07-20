47eee020169db4f56ff13fb0a21c1fb285cfb1ea
John Saputo <jsaputo1@gmail.com>
2020-07-20 23:08:10 +0000

    added question and answer to the question class

--- question.rb
+++ question.rb
@@ -1,1 +1,23 @@
-puts "question working"
+class Question
+
+  attr_accessor :num1, :num2
+
+  def initialize
+    @num1=rand(1..20)
+    @num2=rand(1..20)
+  end
+
+  def question
+    "What does #{@num1} plus #{@num2} equal?"
+  end
+
+  def answer
+    @num1+@num2
+  end
+
+  # test = Question.new
+  # puts test.question
+  # puts test.answer
+
+end
+
